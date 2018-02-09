cc28424b063a6aab4f490715a1deed0f5be56624
Ayush Goyal <ayush987goyal@gmail.com>
2018-02-10 01:21:30 +0530

    added post list component and refactored other pages

---
+++ components/Posts/PostList.vue
@@ -0,0 +1,41 @@
+<template>
+  <section class="post-list">
+    <PostPreview
+      id="1"
+      title="Hello there!"
+      previewText="This is my first post!"
+      thumbnail="https://i.investopedia.com/image/jpeg/1509206221062/tech_global_shutterstock_134102588.jpg"/>
+    <PostPreview
+      id="2"
+      title="Hello there! - the second time!"
+      previewText="This is my second post!"
+      thumbnail="https://i.investopedia.com/image/jpeg/1509206221062/tech_global_shutterstock_134102588.jpg"/>
+    <PostPreview
+      id="3"
+      title="Hi!"
+      previewText="This is my third post!"
+      thumbnail="https://i.investopedia.com/image/jpeg/1509206221062/tech_global_shutterstock_134102588.jpg"/>
+  </section>
+</template>
+
+<script>
+import PostPreview from '~/components/Posts/PostPreview';
+
+export default {
+  components: {
+    PostPreview
+  }
+};
+</script>
+
+<style scoped>
+.post-list {
+  display: flex;
+  padding: 20px;
+  box-sizing: border-box;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+}
+</style>
+
--- pages/index.vue
+++ pages/index.vue
@@ -3,32 +3,16 @@
     <section class="intro">
       <h1>Get the latest tech news!</h1>
     </section>
-    <section class="featured-posts">
-      <PostPreview
-        id="1"
-        title="Hello there!"
-        previewText="This is my first post!"
-        thumbnail="https://i.investopedia.com/image/jpeg/1509206221062/tech_global_shutterstock_134102588.jpg"/>
-      <PostPreview
-        id="2"
-        title="Hello there! - the second time!"
-        previewText="This is my second post!"
-        thumbnail="https://i.investopedia.com/image/jpeg/1509206221062/tech_global_shutterstock_134102588.jpg"/>
-      <PostPreview
-        id="3"
-        title="Hi!"
-        previewText="This is my third post!"
-        thumbnail="https://i.investopedia.com/image/jpeg/1509206221062/tech_global_shutterstock_134102588.jpg"/>
-    </section>
+    <PostList />
   </div>
 </template>
 
 <script>
-import PostPreview from '~/components/Posts/PostPreview';
+import PostList from '~/components/Posts/PostList';
 
 export default {
   components: {
-    PostPreview
+    PostList
   }
 };
 </script>
@@ -64,14 +48,5 @@ export default {
   .intro h1 {
     font-size: 2rem;
   }
-}
-
-.featured-posts {
-  display: flex;
-  padding: 20px;
-  box-sizing: border-box;
-  flex-wrap: wrap;
-  align-items: center;
-  justify-content: center;
 }
 </style>
