672b6cccb6ad61fe998c57b294be25067b4e807c
Ayush Goyal <ayush987goyal@gmail.com>
2018-02-10 20:27:02 +0530

    adding some ui components and admin section

--- components/Posts/PostList.vue
+++ components/Posts/PostList.vue
@@ -2,16 +2,19 @@
   <section class="post-list">
     <PostPreview
       id="1"
+      :is-admin="this.isAdmin"
       title="Hello there!"
       previewText="This is my first post!"
       thumbnail="https://i.investopedia.com/image/jpeg/1509206221062/tech_global_shutterstock_134102588.jpg"/>
     <PostPreview
       id="2"
+      :is-admin="this.isAdmin"
       title="Hello there! - the second time!"
       previewText="This is my second post!"
       thumbnail="https://i.investopedia.com/image/jpeg/1509206221062/tech_global_shutterstock_134102588.jpg"/>
     <PostPreview
       id="3"
+      :is-admin="this.isAdmin"
       title="Hi!"
       previewText="This is my third post!"
       thumbnail="https://i.investopedia.com/image/jpeg/1509206221062/tech_global_shutterstock_134102588.jpg"/>
@@ -24,6 +27,12 @@ import PostPreview from '~/components/Posts/PostPreview';
 export default {
   components: {
     PostPreview
+  },
+  props: {
+    isAdmin: {
+      type: Boolean,
+      default: false
+    }
   }
 };
 </script>
